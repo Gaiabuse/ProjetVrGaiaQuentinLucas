1edd62f2cbb498b31bedd27c65652f2e9746c21d
combault gaia <combaultgaia@gmail.com>
2025-10-02 17:15:57 +0200

    bon je recommence c'etait pas bien fais

--- Assets/Scripts/Data/Visuals/DialogueNode.cs
+++ Assets/Scripts/Data/Visuals/DialogueNode.cs
@@ -1,14 +1,20 @@
 using UnityEngine;
 using XNode;
 
+[CreateAssetMenu]
 public class DialogueNode : Node
 {
+    [Input] public Node Previous;
+    [Output(dynamicPortList = true)] public Node Next;
+    
+    
     public string Title;
     [TextArea(3, 6)] public string Text;
 
     [Output(dynamicPortList = true, connectionType = ConnectionType.Multiple)]
     public Response[] Responses;
 
+    [HideInInspector] public Vector2 Size = new Vector2(300,150);
     public DialogueNode GetNextNode(int index)
     {
         var port = GetOutputPort("Responses" + index);
@@ -18,4 +24,5 @@ public class DialogueNode : Node
         }
         return null;
     }
+    
 }

6e2b580828283d2d4e1db28e34c49d9175dfa4e9
combault gaia <combaultgaia@gmail.com>
2025-10-08 16:08:25 +0200

    tools dialogue node (j'avais oublie de push

---
+++ Assets/Scripts/XNode/DialogueNode.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using XNode;
+
+public class DialogueNode : BaseNode
+{
+    [Input] public string Entry;
+    [Output(dynamicPortList = true)] public string[] Choices;
+
+    public string SpeakerName;
+    [TextArea(4,Int32.MaxValue)]public string DialogueLine;
+
+    public override string GetString()
+    {
+        return "DialogueNode/" + SpeakerName + "/" + DialogueLine;
+    }
+}
